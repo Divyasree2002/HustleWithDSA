9f3abeee3d909439a4feb04e066d4a30c14c0980
V.Divya Sree <76731589+Divyasree2002@users.noreply.github.com>
2023-07-19 22:00:46 +0530

    Create Day 1- LinearSearch.java
    Program on Linear Search - Java

---
+++ Day 1- LinearSearch.java
@@ -0,0 +1,18 @@
+public class LinearSearch
+{
+    private static void linearSearch(int arr[], int num){
+        for(int i = 0;i<arr.length;i++){
+            if(arr[i]==num){
+                System.out.println(num + " is found at " + i);
+                return;
+            }
+        }
+        System.out.println("number not found");
+    }
+	public static void main(String[] args) {
+		int[] arr = {10,43,65,12,6,3};
+		int num = 6;
+		
+		linearSearch(arr, num);
+	}
+}
