50e7610559efa685e659020d1912f0dc1425c667
V.Divya Sree <76731589+Divyasree2002@users.noreply.github.com>
2023-07-20 17:13:47 +0530

    Create Day 2 - BinarySearch.java

---
+++ Day 2 - BinarySearch.java
@@ -0,0 +1,28 @@
+public class BinarySearch
+{
+    public static int bSearch(int arr[], int start, int end, int elem){
+        if(start <= end) {
+          int mid = start + ((end-start)/2);
+          if(arr[mid] == elem)
+              return mid ;
+          else if(arr[mid] > elem)
+              return bSearch(arr, start, mid-1, elem);
+          else
+              return bSearch(arr, mid+1, end, elem);
+        }
+        else
+            return -1;
+    }
+
+	  public static void main(String[] args) {
+		int arr[] = {10,20,30,40,50,60,70,80,90};
+		int elem = 60;
+		int n = arr.length;
+		int pos = bSearch(arr, 0, n-1, elem);
+		if(pos == -1)
+		    System.out.println("Element is not found");
+		
+		else
+		    System.out.println("The element " + elem + " is found at " + pos);
+	  }		
+}
